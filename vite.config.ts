0257e1a8ebf18c37bc1285f74216e2755ad8a9ce
ahmad badreldin <ahmadbaderldin10@gmail.com>
2025-05-05 09:49:42 +0300

    prepare for vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Make sure this is '/' for Vercel
   server:{
     host: true,
     port: 5173
